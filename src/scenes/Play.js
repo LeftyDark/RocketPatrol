1220fa9bf6b10ae9d70adc4633bf14dc07dd75ea
LeftyDark <rysmccar@ucsc.edu>
2021-06-25 18:50:23 -0700

    Update Rocket and Add Spaceships

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -21,10 +21,34 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0,game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0,0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize,0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
+        
+        this.p1Rocket = new Rocket(this, 
+            game.config.width /2, game.config.height - (borderUISize +borderPadding), 
+            'rocket').setOrigin(0.5,0); 
+        //please let this end the rocket so it stops eating up the next part of the code and breaking the program
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6,
+           borderUISize*4, 'spaceship', 0, 30).setOrigin(0,0);
+        //end of spaceship 1
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3,
+            borderUISize*5 + borderPadding, 'spaceship', 0, 20).setOrigin(0,0);
+        //end of spaceship 2
+        this.ship03 = new Spaceship(this, game.config.width,
+            borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+        //end of spaceship3
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        
+        
     
-        this.p1Rocket = new Rocket(this, game.config.width /2, game.config.height - (borderUISize +borderPadding), 'rocket').setOrigin(0.5,0);
+        
     }
     update() {
         this.starfield.tilePositionX -=  4;
+        this.p1Rocket.update();
+        this.ship01.update();
+        this.ship02.update();
+        this.ship03.update();
     }
 }
